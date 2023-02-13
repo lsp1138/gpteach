8bfebccc63c753896c1ef232e2806b7d5e1f788e
lsp1138 <larspittman@gmail.com>
2023-02-13 19:42:55 +0000

    working integration with openapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,50 @@
 import os
 
+import openai
 import requests
 from app.config import Settings, get_settings, log
 from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+
+
+class Prompt(BaseModel):
+    question: str
+    botResponse: str = ""
+
 
 app = FastAPI()
 
 
-@app.get("/prompts/")
-async def prompts(settings: Settings = Depends(get_settings)):
+@app.get("/prompts")
+async def get_prompts(Settings: Settings = Depends(get_settings)):
+    log.info("Getting prompts")
+    return {"question": "a question", "botResponse": "a bot response"}
 
-    log.info("In the prompts endpoint")
 
-    return {
-        "ping": "pong!",
-        "environment": settings.environment,
-        "testing": settings.testing,
-    }
+@app.post("/prompts")
+async def create_prompt(prompt: Prompt, settings: Settings = Depends(get_settings)):
+
+    log.info(f"Prompt question {prompt.question}")
+
+    openai.api_key = settings.openai_api_key
+
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=generate_prompt(prompt.question),
+        temperature=0.6,
+    )
+
+    return response.choices[0].text
+
+
+def generate_prompt(animal):
+    return """Suggest three names for an animal that is a superhero.
+
+Animal: Cat
+Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
+Animal: Dog
+Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: {}
+Names:""".format(
+        animal.capitalize()
+    )
