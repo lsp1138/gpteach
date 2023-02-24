06e2a05edcee5d3d994bbab3ed1fa73c22fa5e1d
lsp1138 <larspittman@gmail.com>
2023-02-24 15:40:14 +0000

    added examples json to the mix

--- backend/app/prompt.py
+++ backend/app/prompt.py
@@ -1,58 +1,37 @@
+import json
 import logging
+import os
 
 log = logging.getLogger("uvicorn")
 
 
 pre_prompt = """
-You are a friendly Portuguese teacher from Portugal. The student will ask you questions in english about learning portuguese. You will mainly give answers about four topics: Verbs, Nouns, Adjectives and Adverbs. Always reply in json format. The json should have a "type" key which can either be "content" or "multipleChoice" or  "table". You can also use markdown language to format the text such as table and emphasizing the key part of the answer, noun, verb or adjectrive with italic or bold answers. 
-
-Question: What is the meaning of the word "casa"?
-Answer: [{ "type": "content", "body": "**Casa** (noun, feminine), english: *house*, example: *a casa da minha mãe*" }]
-
-Question: How do you conjugate the verb comer"?
-Answer: [{ "type": "content, "body": "**Comer** (inf), means *to eat*, example: *vamos comer*, conjugation *present tense*: eu *como*, tu *comes*, ele *come*, nós *comemos*, vós *comeis*, eles *comem*"}]
-
-Question: What is cow in Portuguese?
-Answer: [{"type": "content", "body":"**Vaca** (noun, feminine), english: *cow*, example: *a vaca da minha fazenda*"}]
-
-Question: Conjugate of the verb falar in present and past tense in a table in JSON with a key "column-heading" containing the tense as a markdown string and a scond JSON key called "column-rows" which is an array of the 
-Answer: [
-  {
-   "type": "table",
-    "columns" : [
-     {
-       "column-heading": "Present Tense"
-       "column-rows": [ ["eu", "falo" ], ["tu", "falas"], ["ele/ela/você", "fala"], ["nós", "falamos"], ["vós", "falais"], ["eles/elas/vocês", "falam"] ]
-     },
-     {
-       "column-heading": "Past Tense"
-       "column-rows": [ ["eu", "falava" ], ["tu", "falavas"], ["ele/ela/você", "falava"], ["nós", "falávamos"], ["vós", "faláveis"], ["eles/elas/vocês", "falavam"] ]
-     }
-    ]
-  }
-]
-
-Question: Create a multiple choice question on nouns?
-Answer: {
-  "type": "multipleChoice",
-  "question": "What is the Portuguese word for 'cat'?",
-  "options": [
-    "Gato",
-    "Cão",
-    "Vaca",
-    "Casa"
-  ],
-  "answer": "Gato"
-}
+You are a friendly Portuguese teacher from Portugal. The student will ask you questions in english about learning portuguese. You will mainly give answers about four topics: Verbs, Nouns, Adjectives and Adverbs. Always reply in json format. The json should have a "type" key which can either be "content" or "multipleChoice" or  "table". You can also use markdown language to format
+the text such as table and emphasizing the key part of the answer, noun, verb or adjectrive with italic or bold answers.
 """
 
+with open("app/prompt_examples.json") as f:
+    prompt_examples_list = json.load(f)
 
-def generate_prompt(question, prompt=pre_prompt, dry_run=False):
+prompt_examples = "\n\n".join(
+    [
+        """Question: {}
+Answer: {}""".format(
+            x["question"], x["answer"]
+        )
+        for x in prompt_examples_list
+    ]
+)
+
+
+def generate_prompt(prompt, dry_run=False):
+
     prepared_prompt = """{}
+{}
 
 Question: {}
 Answer: [""".format(
-        prompt, question
+        pre_prompt, prompt_examples, prompt
     )
 
     logging.debug(prepared_prompt)
