8f389b591748ee05d088a1e7fa1dd6e392138ddc
lsp1138 <larspittman@gmail.com>
2023-02-26 21:54:46 +0000

    changed to a table design in side the flex boxes

--- frontend/src/components/FlexTable.js
+++ frontend/src/components/FlexTable.js
@@ -1,31 +1,30 @@
+// A flex box with tables inside for a responsive output
+// of conjugation tables
+
 function TableChoice(body) {
   console.log("body is", body);
 
   return (
-    <div className="container">
-      <div className="row">
-        {body.columns.map((column, indexCol) => (
-          <div key={"columns-" + indexCol} className="col">
-            <div className="row">
-              <b>{column.heading}</b>
-            </div>
-            <div className="row border-right">
-              {column.rows.map((row, indexRow) => (
-                <div key={"row" + indexRow} className="row">
-                  {row.map((rowContent, indexRowContent) => (
-                    <div
-                      key={"row-content-" + indexRowContent}
-                      className="col-6"
-                    >
-                      {rowContent}
-                    </div>
-                  ))}
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="flexTable d-md-flex">
+      {body.columns.map((column, indexCol) => (
+        <div key={"columns-" + indexCol} className="p-1">
+          <table>
+            <tr>
+              <th colspan="2">{column.heading}</th>
+            </tr>
+            {column.rows.map((row, indexRow) => (
+              <tr>
+                <td>
+                  <i>{row[0]}</i>
+                </td>
+                <td>
+                  <>{row[1]}</>
+                </td>
+              </tr>
+            ))}
+          </table>
+        </div>
+      ))}
     </div>
   );
 }
